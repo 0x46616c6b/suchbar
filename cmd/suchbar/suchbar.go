33a097a0e01618a1debebe81c1054c04c7b0aa0f
louis <louis@systemli.org>
2017-04-24 19:04:59 +0200

    added missing files

---
+++ cmd/suchbar/suchbar.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"flag"
+
+	log "github.com/Sirupsen/logrus"
+)
+
+var (
+	since      string
+	until      string
+	limit      int
+	configPath string
+	config     Config
+)
+
+func init() {
+	flag.StringVar(&since, "facebook.since", "24h", "the earliest date for fetching posts")
+	flag.StringVar(&until, "facebook.until", "", "the latest date for fetching posts")
+	flag.IntVar(&limit, "facebook.limit", 100, "the limit for fetching posts per iteration")
+	flag.StringVar(&configPath, "config", "config.yml", "path to the configuration file")
+	flag.Parse()
+
+	config.read(configPath)
+
+	level, err := log.ParseLevel(config.LogLevel)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.SetLevel(level)
+}
+
+func main() {
+	r := NewRunner(config)
+	r.Run()
+}
